b388a3daa7097426a2d885bfac7270ab9489035a
AlbertEMC2Stein <prokosch@rhrk.uni-kl.de>
2023-10-13 16:07:46 +0200

    updated documentation

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,8 @@
 URLS=[
-"index.html"
+"index.html",
+"PyReport/Symbols/index.html",
+"PyReport/Symbols/greek.html",
+"PyReport/Symbols/hebrew.html"
 ];
 INDEX=[
 {
@@ -8,9 +11,286 @@ INDEX=[
 "doc":"PyReport - A Python package for creating LaTeX reports from within your code!"
 },
 {
+"ref":"PyReport.ReportMaker",
+"url":0,
+"doc":""
+},
+{
+"ref":"PyReport.ReportMaker.make",
+"url":0,
+"doc":"",
+"func":1
+},
+{
 "ref":"PyReport.get_info",
 "url":0,
 "doc":"",
 "func":1
+},
+{
+"ref":"PyReport.Symbols",
+"url":1,
+"doc":""
+},
+{
+"ref":"PyReport.Symbols.SymbolInterface",
+"url":1,
+"doc":"This class represents the symbol \\([Symbol\\_Command]\\) ([Symbol_Name])."
+},
+{
+"ref":"PyReport.Symbols.SymbolInterface.toTeX",
+"url":1,
+"doc":"",
+"func":1
+},
+{
+"ref":"PyReport.Symbols.greek",
+"url":2,
+"doc":""
+},
+{
+"ref":"PyReport.Symbols.greek.DEFAULT_VAREPSILON",
+"url":2,
+"doc":"Switch out the default LaTeX command for epsilon with the alternate one (varepsilon)."
+},
+{
+"ref":"PyReport.Symbols.greek.DEFAULT_VARTHETA",
+"url":2,
+"doc":"Switch out the default LaTeX command for theta with the alternate one (vartheta)."
+},
+{
+"ref":"PyReport.Symbols.greek.DEFAULT_VARPI",
+"url":2,
+"doc":"Switch out the default LaTeX command for pi with the alternate one (varpi)."
+},
+{
+"ref":"PyReport.Symbols.greek.DEFAULT_VARRHO",
+"url":2,
+"doc":"Switch out the default LaTeX command for rho with the alternate one (varrho)."
+},
+{
+"ref":"PyReport.Symbols.greek.DEFAULT_VARPHI",
+"url":2,
+"doc":"Switch out the default LaTeX command for phi with the alternate one (varphi)."
+},
+{
+"ref":"PyReport.Symbols.greek.alpha",
+"url":2,
+"doc":"This class represents the symbol \\(\\alpha\\) (alpha)."
+},
+{
+"ref":"PyReport.Symbols.greek.beta",
+"url":2,
+"doc":"This class represents the symbol \\(\\beta\\) (beta)."
+},
+{
+"ref":"PyReport.Symbols.greek.gamma",
+"url":2,
+"doc":"This class represents the symbol \\(\\gamma\\) (gamma)."
+},
+{
+"ref":"PyReport.Symbols.greek.delta",
+"url":2,
+"doc":"This class represents the symbol \\(\\delta\\) (delta)."
+},
+{
+"ref":"PyReport.Symbols.greek.epsilon",
+"url":2,
+"doc":"This class represents the symbol \\(\\epsilon\\) (epsilon) or alternatively \\(\\varepsilon\\) depending on DEFAULT_VAREPSILON."
+},
+{
+"ref":"PyReport.Symbols.greek.varepsilon",
+"url":2,
+"doc":"This class represents the symbol \\(\\varepsilon\\) (varepsilon) or alternatively \\(\\epsilon\\) depending on DEFAULT_VAREPSILON."
+},
+{
+"ref":"PyReport.Symbols.greek.zeta",
+"url":2,
+"doc":"This class represents the symbol \\(\\zeta\\) (zeta)."
+},
+{
+"ref":"PyReport.Symbols.greek.eta",
+"url":2,
+"doc":"This class represents the symbol \\(\\eta\\) (eta)."
+},
+{
+"ref":"PyReport.Symbols.greek.theta",
+"url":2,
+"doc":"This class represents the symbol \\(\\theta\\) (theta) or alternatively \\(\\vartheta\\) depending on DEFAULT_VARTHETA."
+},
+{
+"ref":"PyReport.Symbols.greek.vartheta",
+"url":2,
+"doc":"This class represents the symbol \\(\\vartheta\\) (vartheta) or alternatively \\(\\theta\\) depending on DEFAULT_VARTHETA."
+},
+{
+"ref":"PyReport.Symbols.greek.iota",
+"url":2,
+"doc":"This class represents the symbol \\(\\iota\\) (iota)."
+},
+{
+"ref":"PyReport.Symbols.greek.kappa",
+"url":2,
+"doc":"This class represents the symbol \\(\\kappa\\) (kappa)."
+},
+{
+"ref":"PyReport.Symbols.greek.lambda_",
+"url":2,
+"doc":"This class represents the symbol \\(\\lambda\\) (lambda)."
+},
+{
+"ref":"PyReport.Symbols.greek.mu",
+"url":2,
+"doc":"This class represents the symbol \\(\\mu\\) (mu)."
+},
+{
+"ref":"PyReport.Symbols.greek.nu",
+"url":2,
+"doc":"This class represents the symbol \\(\\nu\\) (nu)."
+},
+{
+"ref":"PyReport.Symbols.greek.xi",
+"url":2,
+"doc":"This class represents the symbol \\(\\xi\\) (xi)."
+},
+{
+"ref":"PyReport.Symbols.greek.pi",
+"url":2,
+"doc":"This class represents the symbol \\(\\pi\\) (pi) or alternatively \\(\\varpi\\) depending on DEFAULT_VARPI."
+},
+{
+"ref":"PyReport.Symbols.greek.varpi",
+"url":2,
+"doc":"This class represents the symbol \\(\\varpi\\) (varpi) or alternatively \\(\\pi\\) depending on DEFAULT_VARPI."
+},
+{
+"ref":"PyReport.Symbols.greek.rho",
+"url":2,
+"doc":"This class represents the symbol \\(\\rho\\) (rho) or alternatively \\(\\varrho\\) depending on DEFAULT_VARRHO."
+},
+{
+"ref":"PyReport.Symbols.greek.varrho",
+"url":2,
+"doc":"This class represents the symbol \\(\\varrho\\) (varrho) or alternatively \\(\\rho\\) depending on DEFAULT_VARRHO."
+},
+{
+"ref":"PyReport.Symbols.greek.sigma",
+"url":2,
+"doc":"This class represents the symbol \\(\\sigma\\) (sigma)."
+},
+{
+"ref":"PyReport.Symbols.greek.tau",
+"url":2,
+"doc":"This class represents the symbol \\(\\tau\\) (tau)."
+},
+{
+"ref":"PyReport.Symbols.greek.upsilon",
+"url":2,
+"doc":"This class represents the symbol \\(\\upsilon\\) (upsilon)."
+},
+{
+"ref":"PyReport.Symbols.greek.phi",
+"url":2,
+"doc":"This class represents the symbol \\(\\phi\\) (phi) or alternatively \\(\\varphi\\) depending on DEFAULT_VARPHI."
+},
+{
+"ref":"PyReport.Symbols.greek.varphi",
+"url":2,
+"doc":"This class represents the symbol \\(\\varphi\\) (varphi) or alternatively \\(\\phi\\) depending on DEFAULT_VARPHI."
+},
+{
+"ref":"PyReport.Symbols.greek.chi",
+"url":2,
+"doc":"This class represents the symbol \\(\\chi\\) (chi)."
+},
+{
+"ref":"PyReport.Symbols.greek.psi",
+"url":2,
+"doc":"This class represents the symbol \\(\\psi\\) (psi)."
+},
+{
+"ref":"PyReport.Symbols.greek.omega",
+"url":2,
+"doc":"This class represents the symbol \\(\\omega\\) (omega)."
+},
+{
+"ref":"PyReport.Symbols.greek.Gamma",
+"url":2,
+"doc":"This class represents the symbol \\(\\Gamma\\) (Gamma)."
+},
+{
+"ref":"PyReport.Symbols.greek.Delta",
+"url":2,
+"doc":"This class represents the symbol \\(\\Delta\\) (Delta)."
+},
+{
+"ref":"PyReport.Symbols.greek.Theta",
+"url":2,
+"doc":"This class represents the symbol \\(\\Theta\\) (Theta)."
+},
+{
+"ref":"PyReport.Symbols.greek.Lambda",
+"url":2,
+"doc":"This class represents the symbol \\(\\Lambda\\) (Lambda)."
+},
+{
+"ref":"PyReport.Symbols.greek.Xi",
+"url":2,
+"doc":"This class represents the symbol \\(\\Xi\\) (Xi)."
+},
+{
+"ref":"PyReport.Symbols.greek.Pi",
+"url":2,
+"doc":"This class represents the symbol \\(\\Pi\\) (Pi)."
+},
+{
+"ref":"PyReport.Symbols.greek.Sigma",
+"url":2,
+"doc":"This class represents the symbol \\(\\Sigma\\) (Sigma)."
+},
+{
+"ref":"PyReport.Symbols.greek.Upsilon",
+"url":2,
+"doc":"This class represents the symbol \\(\\Upsilon\\) (Upsilon)."
+},
+{
+"ref":"PyReport.Symbols.greek.Phi",
+"url":2,
+"doc":"This class represents the symbol \\(\\Phi\\) (Phi)."
+},
+{
+"ref":"PyReport.Symbols.greek.Psi",
+"url":2,
+"doc":"This class represents the symbol \\(\\Psi\\) (Psi)."
+},
+{
+"ref":"PyReport.Symbols.greek.Omega",
+"url":2,
+"doc":"This class represents the symbol \\(\\Omega\\) (Omega)."
+},
+{
+"ref":"PyReport.Symbols.hebrew",
+"url":3,
+"doc":""
+},
+{
+"ref":"PyReport.Symbols.hebrew.aleph",
+"url":3,
+"doc":"This class represents the symbol \\(\\aleph\\) (aleph)."
+},
+{
+"ref":"PyReport.Symbols.hebrew.beth",
+"url":3,
+"doc":"This class represents the symbol \\(\\beth\\) (beth)."
+},
+{
+"ref":"PyReport.Symbols.hebrew.daleth",
+"url":3,
+"doc":"This class represents the symbol \\(\\daleth\\) (daleth)."
+},
+{
+"ref":"PyReport.Symbols.hebrew.gimel",
+"url":3,
+"doc":"This class represents the symbol \\(\\gimel\\) (gimel)."
 }
 ]
