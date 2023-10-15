e2b8fba20fd5433d2dc977b28b87b7aa46d64c13
AlbertEMC2Stein <prokosch@rhrk.uni-kl.de>
2023-10-15 21:25:40 +0200

    Linted code

--- docs/makedocs.py
+++ docs/makedocs.py
@@ -1,17 +1,17 @@
 import shutil
 import zipfile
-import os.path as path
-from os import remove, system, listdir
+from os import path, remove, system, listdir
 from pathlib import Path
 from pyreport import ROOT_DIR, get_info
 
-PACKAGE_NAME = get_info()['name']
+PACKAGE_NAME = get_info()["name"]
 PACKAGE_DIR = path.join(ROOT_DIR, PACKAGE_NAME)
 DOCS_DIR = path.dirname(path.abspath(__file__))
-TEMPLATE_DIR = path.join(DOCS_DIR, 'templates')
-BLACKLIST = ['makedocs.py', 'images', 'templates']
+TEMPLATE_DIR = path.join(DOCS_DIR, "templates")
+BLACKLIST = ["makedocs.py", "images", "templates"]
 
 ########################################################################
+
 
 def clear_files():
     for file in listdir(DOCS_DIR):
@@ -21,39 +21,52 @@ def clear_files():
             else:
                 remove(path.join(DOCS_DIR, file))
 
+
 def compress(file_names, output_directory):
     compression = zipfile.ZIP_DEFLATED
 
-    zf = zipfile.ZipFile(path.join(output_directory, "pyreport_documentation.zip"), mode="w")
+    zf = zipfile.ZipFile(
+        path.join(output_directory, "pyreport_documentation.zip"), mode="w"
+    )
 
     try:
         for file_name in file_names:
-            zf.write(path.join(DOCS_DIR, file_name), file_name, compress_type=compression)
+            zf.write(
+                path.join(DOCS_DIR, file_name), file_name, compress_type=compression
+            )
     except FileNotFoundError as e:
-        raise RuntimeError("An error occured while compressing the documentation.")
+        message = "An error occured while compressing the documentation."
+        raise RuntimeError(message) from e
     finally:
         zf.close()
 
 
 def modify_docfiles():
-    with open(path.join(DOCS_DIR, PACKAGE_NAME, "index.html"), "r") as f:
+    with open(
+        path.join(DOCS_DIR, PACKAGE_NAME, "index.html"), "r", encoding="utf8"
+    ) as f:
         html = f.read()
         html = html.replace("../", "./")
 
-        modules = [module.name for module in Path(DOCS_DIR, PACKAGE_NAME).rglob("*") if module.name != 'index.html']
+        modules = [
+            module.name
+            for module in Path(DOCS_DIR, PACKAGE_NAME).rglob("*")
+            if module.name != "index.html"
+        ]
+
         for module in modules:
             html = html.replace(f'href="{module}"', f'href="{PACKAGE_NAME}/{module}"')
 
-        with open(path.join(DOCS_DIR, "index.html"), "w") as f:
-                    f.write(html)
+        with open(path.join(DOCS_DIR, "index.html"), "w", encoding="utf8") as f:
+            f.write(html)
 
     remove(path.join(DOCS_DIR, PACKAGE_NAME, "index.html"))
 
-    with open(path.join(DOCS_DIR, "index.js"), "r") as f:
+    with open(path.join(DOCS_DIR, "index.js"), "r", encoding="utf8") as f:
         js = f.read()
         js = js.replace(f"{PACKAGE_NAME}/index.html", "index.html")
 
-        with open(path.join(DOCS_DIR, "index.js"), "w") as f:
+        with open(path.join(DOCS_DIR, "index.js"), "w", encoding="utf8") as f:
             f.write(js)
 
     for file in listdir(path.join(DOCS_DIR, PACKAGE_NAME)):
@@ -64,10 +77,14 @@ def modify_docfiles():
 
 ########################################################################
 
+
 def build(output_directory, keep_temporary_files):
     try:
         clear_files()
-        system(f'pdoc3 --force --html --template-dir "{TEMPLATE_DIR}" --output-dir "{DOCS_DIR}" "{PACKAGE_DIR}"')
+        system(
+            f'pdoc3 --force --html --template-dir "{TEMPLATE_DIR}" \
+               --output-dir "{DOCS_DIR}" "{PACKAGE_DIR}"'
+        )
         modify_docfiles()
     except RuntimeError:
         print("An error occured while generating the documentation.")
@@ -76,15 +93,15 @@ def build(output_directory, keep_temporary_files):
     try:
         start_at = len(DOCS_DIR) + 1
         file_names = ["index.js"] + [
-                                        str(file)[start_at:] 
-                                        for file in Path(DOCS_DIR).rglob("*") 
-                                        if file.name.endswith(".html")
-                                    ]
+            str(file)[start_at:]
+            for file in Path(DOCS_DIR).rglob("*")
+            if file.name.endswith(".html")
+        ]
 
         if len(file_names) == 0:
             raise RuntimeError("No files were found in the documentation directory.")
-      
-        print('All files:\n\t-', "\n\t- ".join(file_names)) 
+
+        print("All files:\n\t-", "\n\t- ".join(file_names))
         compress(file_names, output_directory)
     except RuntimeError as e:
         print(e)
@@ -93,4 +110,3 @@ def build(output_directory, keep_temporary_files):
         shutil.rmtree(path.join(DOCS_DIR, PACKAGE_NAME))
         if not keep_temporary_files:
             clear_files()
-        
