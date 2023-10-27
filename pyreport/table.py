ffc2989925a1f4f971e8431180ea05116404f29f
AlbertEMC2Stein <prokosch@rhrk.uni-kl.de>
2023-10-27 09:22:09 +0200

    Changed indentation

--- pyreport/table.py
+++ pyreport/table.py
@@ -36,9 +36,14 @@ class Table(LaTeXObject):
 
     def texify(self, file, indent_level=0):
         tex = "\\begin{table}[ht]\n"
-        tex += "\\centering\n"
-        tex += self._data.to_latex(index=False)
-        tex += f"\\caption{{{self._caption}}}\n" if self._caption else ""
-        tex += f"\\label{{tab:{self._label}}}\n" if self._label else ""
+        tex += "\t\\centering\n"
+
+        lines = self._data.to_latex(index=False).split("\n")[:-1]
+        for i, line in enumerate(lines):
+            bodyindent = "\t" if i not in [0, len(lines) - 1] else ""
+            tex += "\t" + bodyindent + line + "\n"
+
+        tex += f"\t\\caption{{{self._caption}}}\n" if self._caption else ""
+        tex += f"\t\\label{{tab:{self._label}}}\n" if self._label else ""
         tex += "\\end{table}\n"
         indented_write(file, indent_level, tex)
